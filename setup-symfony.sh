e6049c9648d5814245d9875ad2f3d0b64e95d66c
Slloth <fiquet.noah@gmail.com>
2021-12-21 11:16:46 +0100

    mise en cache des fichier var et vendor

---
+++ setup-symfony.sh
@@ -0,0 +1,2 @@
+[ -e /var/www/DefautSymfonyApli/var ] || ln -s /var/www/var /var/www/DefautSymfonyApli/var
+[ -e /var/www/DefautSymfonyApli/vendor ] || ln -s /var/www/vendor /var/www/DefautSymfonyApli/vendor
