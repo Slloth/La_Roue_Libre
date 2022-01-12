469372dbb65d873f0be417826fa604e118ece0b1
Slloth <fiquet.noah@gmail.com>
2022-01-12 14:28:38 +0100

    Architecture docker MàJ

--- setup-symfony.sh
+++
@@ -1,2 +0,0 @@
-[ -e /var/www/DefautSymfonyApli/var ] || ln -s /var/www/var /var/www/DefautSymfonyApli/var
-[ -e /var/www/DefautSymfonyApli/vendor ] || ln -s /var/www/vendor /var/www/DefautSymfonyApli/vendor
