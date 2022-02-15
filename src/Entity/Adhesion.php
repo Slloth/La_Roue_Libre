1932540be55856758b297ccf18b8c77b9e04ed91
Pierre Devaux <helprax@gmail.com>
2022-02-15 16:50:26 +0100

    Creation des entités

---
+++ src/Entity/Adhesion.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdhesionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AdhesionRepository::class)
+ */
+class Adhesion
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\OneToMany(targetEntity=SouscriptionAdhesion::class, mappedBy="adhesions")
+     */
+    private $souscriptionAdhesions;
+
+    public function __construct()
+    {
+        $this->souscriptionAdhesions = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|SouscriptionAdhesion[]
+     */
+    public function getSouscriptionAdhesions(): Collection
+    {
+        return $this->souscriptionAdhesions;
+    }
+
+    public function addSouscriptionAdhesion(SouscriptionAdhesion $souscriptionAdhesion): self
+    {
+        if (!$this->souscriptionAdhesions->contains($souscriptionAdhesion)) {
+            $this->souscriptionAdhesions[] = $souscriptionAdhesion;
+            $souscriptionAdhesion->setAdhesions($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSouscriptionAdhesion(SouscriptionAdhesion $souscriptionAdhesion): self
+    {
+        if ($this->souscriptionAdhesions->removeElement($souscriptionAdhesion)) {
+            // set the owning side to null (unless already changed)
+            if ($souscriptionAdhesion->getAdhesions() === $this) {
+                $souscriptionAdhesion->setAdhesions(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/SouscriptionAdhesion.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SouscriptionAdhesionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SouscriptionAdhesionRepository::class)
+ */
+class SouscriptionAdhesion
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $InscriptionAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Adherent::class, inversedBy="souscriptionAdhesions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $adherents;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Adhesion::class, inversedBy="souscriptionAdhesions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $adhesions;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInscriptionAt(): ?\DateTimeImmutable
+    {
+        return $this->InscriptionAt;
+    }
+
+    public function setInscriptionAt(\DateTimeImmutable $InscriptionAt): self
+    {
+        $this->InscriptionAt = $InscriptionAt;
+
+        return $this;
+    }
+
+    public function getAdherents(): ?Adherent
+    {
+        return $this->adherents;
+    }
+
+    public function setAdherents(?Adherent $adherents): self
+    {
+        $this->adherents = $adherents;
+
+        return $this;
+    }
+
+    public function getAdhesions(): ?Adhesion
+    {
+        return $this->adhesions;
+    }
+
+    public function setAdhesions(?Adhesion $adhesions): self
+    {
+        $this->adhesions = $adhesions;
+
+        return $this;
+    }
+}
