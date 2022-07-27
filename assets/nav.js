90b60ac3a2f9d195eb1719d3575994c6e5a6e957
Slloth <fiquet.noah@gmail.com>
2022-07-27 15:19:42 +0200

    suppression d'un console log

--- assets/nav.js
+++ assets/nav.js
@@ -3,7 +3,6 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     let navbar = document.querySelector("#navbar");
     if(window.scrollY < 1000  && window.scrollY > 0 && document.querySelector("video")){
-        console.log(window.scrollY);
         navbar.classList.remove("bg-light");
     }
     else{
